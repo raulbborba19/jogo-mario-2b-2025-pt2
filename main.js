2b7bf9727c1c191b6d8b009f4893c536bea016fa
raulbborba19 <raul.borba@escola.pr.gov.br>
2025-06-16 07:43:09 -0300

    .

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 const mario = document.querySelector(".mario")
 const pipe = document.querySelector(".pipe")
+const starButton = document.querySelector(".start")
+const gameOverScreen = document.querySelector(".game-over")
+
+let gameStarted = false;
+
+const starGame = () => {
+    gameStarted = true;
+
+    pipe.style.animation = "pipe-animation 1.5s infine linear";
+
+    startButton.style.display = "none";
+}
+
 
 const jump = () => {
     mario.classList.add("jump");
@@ -29,3 +42,8 @@ const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
     }
 },10);
 document.addEventListener("keydown", jump);
+
+
+const restartGame = () => {
+    window.location.reload();
+}
